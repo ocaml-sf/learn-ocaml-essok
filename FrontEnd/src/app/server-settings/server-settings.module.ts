a5b0ec06df74a759805f1b2e898a17af9e569c6c
Astyax NOUREL <astyax.nourel@etu-univ-paris-diderot.fr>
2019-09-13 19:25:00 +0200

    Added Drag-Drop for Exercises

--- FrontEnd/src/app/server-settings/server-settings.module.ts
+++ FrontEnd/src/app/server-settings/server-settings.module.ts
@@ -1,5 +1,7 @@
 import { ModuleWithProviders, NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import { ScrollingModule } from '@angular/cdk/scrolling'; 
 import { SharedModule } from '../shared';
 import { ServerSettingsComponent } from './server-settings.component';
 import { ServerSettingsRoutingModule } from './server-settings-routing.module';
@@ -10,6 +12,8 @@ import { FileSelectDirective } from 'ng2-file-upload';
   imports: [
     SharedModule,
     ServerSettingsRoutingModule,
+    DragDropModule,
+    ScrollingModule
   ],
   declarations: [
     ServerSettingsComponent,
