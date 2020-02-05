f5570e3b191051aaa06648965f3ae0d1ad19ddf3
Astyax NOUREL <astyax.nourel@etu-univ-paris-diderot.fr>
2020-02-05 14:26:53 +0100

    added another regexp for token

--- BackEnd/lib/global_functions.js
+++ BackEnd/lib/global_functions.js
@@ -1,7 +1,7 @@
-const before_token_regexp1 = '';
-const before_token_regexp1_len = before_token_regexp1.length;
-const before_token_regexp2 = 'Found the following teacher tokens:\n  - ';
-const before_token_regexp2_len = before_token_regexp2.length;
+const token_regexp1 = 'Initial teacher token created: ';
+const token_regexp1_len = token_regexp1.length;
+const token_regexp2 = 'Found the following teacher tokens:\n  - ';
+const token_regexp2_len = token_regexp2.length;
 const token_len = 17;
 
 function _asyncFunction(item, cb) {
@@ -11,11 +11,17 @@ function _asyncFunction(item, cb) {
     }, 100);
 }
 
-function _findTeacherToken(log) {
-    var index = log.indexOf(before_token_regexp2);
+function _findTeacherToken(log, regexp, regexp_len) {
+    var index = log.indexOf(regexp);
     if(index === -1)
 	return undefined;
-    return log.substr(index + before_token_regexp2_len, token_len);
+    return log.substr(index + regexp, token_len);
+}
+
+function _findTeacherToken(log) {
+    var try1 = _findTeacherToken(log, token_regexp1, token_regexp1_len);
+    if(try1 === undefined)
+	return _findTeacherToken(log, token_regexp2, token_regexp2_len);
 }
 
 var global_functions = {
