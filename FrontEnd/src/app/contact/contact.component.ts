d8a15ff0aa6cec9c3c4a296457b526817fc5f6cc
Astyax Nourel <astyax.nourel@etu.univ-paris-diderot.fr>
2020-09-25 15:57:38 +0200

    updating contact page

--- FrontEnd/src/app/contact/contact.component.ts
+++ FrontEnd/src/app/contact/contact.component.ts
@@ -4,8 +4,6 @@ import { ActivatedRoute, Router } from '@angular/router';
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.scss']
-
 })
 export class ContactComponent implements OnInit {
   title: String = '';
