c3df78312263d79b15c2e6683b8b564fde2b755d
Astyax NOUREL <astyax.nourel@etu-univ-paris-diderot.fr>
2020-11-06 02:09:32 +0100

    fixed FrontEnd Http error at start

--- FrontEnd/src/app/core/services/file.service.ts
+++ FrontEnd/src/app/core/services/file.service.ts
@@ -1,16 +1,14 @@
 import { Injectable } from '@angular/core';
-import { HttpHeaders, HttpClient, HttpParams, HttpResponse, HttpEvent, HttpEventType, HttpErrorResponse } from '@angular/common/http';
 
-import { Http, ResponseContentType } from '@angular/http';
-import { Observable, throwError } from 'rxjs';
+import { Observable } from 'rxjs';
 import { ApiService } from './api.service';
-import { map, tap, last, catchError } from 'rxjs/operators';
+import { map } from 'rxjs/operators';
 @Injectable({ providedIn: 'root' })
 export class FileService {
 
     constructor(private apiService: ApiService) { }
 
-    downloadFile(slug, target): Observable<any> {
+    downloadFile(slug: string, target : Object): Observable<any> {
 
         return this.apiService
             .download('/uploads/download/' + slug, { target: target })
