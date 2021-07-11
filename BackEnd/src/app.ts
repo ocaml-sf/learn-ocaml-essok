59787c3e531874ec3682d9385cf025d0443bfd99
Astyax Nourel <astyax.nourel@labusemagique.com>
2021-07-12 01:16:49 +0200

    migrating to Typescript

---
+++ BackEnd/src/app.ts
@@ -0,0 +1,107 @@
+import express from 'express';
+import { AddressInfo } from 'net';
+
+const https = require('https'),
+  path = require('path'),
+  methods = require('methods'),
+  bodyParser = require('body-parser'),
+  session = require('express-session'),
+  cors = require('cors'),
+  passport = require('passport'),
+  errorhandler = require('errorhandler'),
+  mongoose = require('mongoose'),
+  helmet = require('helmet'),
+  fs = require('fs')
+// options = {
+//   key: fs.readFileSync('/.pem', 'utf8'),
+//   cert: fs.readFileSync('/.pem', 'utf8'),
+//   dhparam: fs.readFileSync('/.pem', 'utf8')
+// }
+
+var isProduction = process.env.NODE_ENV === 'production';
+
+// Create global app object
+var app = express();
+
+app.use(cors());
+
+// Normal express config defaults
+app.use(require('morgan')('dev'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(helmet());
+app.disable('x-powered-by');
+app.use(require('method-override')());
+//app.use(express.static('dist/'));
+// add secret inputline to use memory based secret
+app.use(session({ secret: 'essok', cookie: { maxAge: 60000 }, resave: false, saveUninitialized: false }));
+
+if (!isProduction) {
+  app.use(errorhandler());
+}
+
+if (isProduction) {
+  mongoose.connect(process.env.MONGODB_URI);
+} else {
+  mongoose.connect('mongodb://localhost/essok', { useNewUrlParser: true, useUnifiedTopology: true });
+  mongoose.set('useCreateIndex', true);
+  mongoose.set('debug', true);
+}
+
+require('./models/User');
+require('./models/Server');
+require('./models/Log');
+require('./configs/passport');
+
+/// catch 404 and forward to error handler
+app.use(function (_req, res, next) {
+  //set headers to allow cross origin request.
+  res.header("Access-Control-Allow-Origin", 'http://localhost:4200');
+  res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.setHeader('Access-Control-Allow-Credentials', "true");
+  next();
+});
+
+app.use(require('./routes'));
+
+/// error handlers
+
+// development error handler
+// will print stacktrace
+if (!isProduction) {
+  app.use(function (err : any, _req : express.Request,
+                    res : express.Response, _next: express.NextFunction) {
+    console.log(err.stack);
+
+    res.status(err.status || 500);
+
+    res.json({
+      'errors': {
+        message: err.message,
+        error: err
+      }
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function (err : any, _req : express.Request,
+                  res : express.Response, _next : express.NextFunction) {
+  res.status(err.status || 500);
+  res.json({
+    'errors': {
+      message: err.message,
+      error: {}
+    }
+  });
+});
+
+// finally, let's start our server...
+var server = app.listen(process.env.PORT || 3000, function () {
+  console.log('Listening on port ' + (server.address() as AddressInfo).port);
+});
+server.setTimeout(30 * 60 * 1000);
+
+// https.createServer(options, app).listen(8080);
---
+++ BackEnd/src/models/Server.ts
@@ -0,0 +1,50 @@
+var mongoose = require('mongoose');
+var uniqueValidator = require('mongoose-unique-validator');
+var slug = require('slug');
+const domain = 'learn-ocaml.org';
+
+var ServerSchema = new mongoose.Schema({
+  slug: { type: String, lowercase: true, unique: true },
+  title: { type: String, lowercase: true, unique: true, required: [true, "can't be blank"], match: [/^[a-zA-Z0-9]+$/, 'is invalid'], index: true },
+  description: String,
+  body: String,
+  vue: String,
+  volume: String,
+  active: { type: Boolean, default: false },
+  author: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  token: String,
+  url: String
+}, { timestamps: true });
+
+ServerSchema.plugin(uniqueValidator, { message: 'is already taken' });
+
+ServerSchema.pre('validate', function (next) {
+  if (!this.slug) {
+    this.slugify();
+  }
+  this.url = this.author.username + '.' + this.slug + '.' + domain;
+  next();
+});
+
+ServerSchema.methods.slugify = function () {
+  // this.slug = slug(this.title) + '-' + (Math.random() * Math.pow(36, 6) | 0).toString(36);
+  this.slug = slug(this.title);
+};
+
+ServerSchema.methods.toJSONFor = function (user) {
+  return {
+    slug: this.slug,
+    title: this.title,
+    description: this.description,
+    body: this.body,
+    createdAt: this.createdAt,
+    updatedAt: this.updatedAt,
+    author: this.author.toProfileJSONFor(user),
+    active: this.active,
+    volume: this.volume,
+    token: this.token,
+    url: this.url
+  };
+};
+
+mongoose.model('Server', ServerSchema);
